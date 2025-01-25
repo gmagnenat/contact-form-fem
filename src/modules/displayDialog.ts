bcaf3ef942be1a72ac96704bd33ef961c8e94da2
Gwenaël Magnenat <g.magnenat@gmail.com>
2025-01-25 12:57:36 +0100

    Fix dialog size

--- src/modules/displayDialog.ts
+++ src/modules/displayDialog.ts
@@ -1,6 +1,9 @@
 export function displayDialog(dialog: HTMLDialogElement): void {
   dialog.showModal();
 
+  // scroll to the top of the page
+  window.scrollTo(0, 0);
+
   dialog.querySelector(".close")?.addEventListener("click", () => {
     dialog.close();
   });
