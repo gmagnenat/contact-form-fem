b6551742bc70e543c37bb3b302e47f6ea7594dc5
Gwenaël Magnenat <g.magnenat@gmail.com>
2025-01-26 14:32:52 +0100

    Fix dialog issues with eventlisteners and button styling

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,9 @@ import { handleSubmit } from "./modules/formHandler";
 function init() {
   window.addEventListener("DOMContentLoaded", () => {
     const elements = getDOMElements();
+    const closeButton = elements.dialog.querySelector(
+      ".btn--dialog-close"
+    ) as HTMLButtonElement;
 
     const { form } = elements;
     const inputs = form.querySelectorAll("input, textarea");
@@ -18,6 +21,16 @@ function init() {
     form.addEventListener("submit", (event: SubmitEvent) =>
       handleSubmit(event, elements)
     );
+
+    elements.dialog.addEventListener("show", () => {
+      closeButton.focus();
+    });
+
+    document.addEventListener("keydown", (event) => {
+      if (event.key === "Escape" && elements.dialog.open) {
+        elements.dialog.close();
+      }
+    });
   });
 }
 
--- src/modules/displayDialog.ts
+++ src/modules/displayDialog.ts
@@ -4,7 +4,7 @@ export function displayDialog(dialog: HTMLDialogElement): void {
   // scroll to the top of the page
   window.scrollTo(0, 0);
 
-  dialog.querySelector(".close")?.addEventListener("click", () => {
+  dialog.querySelector(".btn--dialog-close")?.addEventListener("click", () => {
     dialog.close();
   });
 }
